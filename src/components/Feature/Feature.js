f1d4aa8693e14bf0e3540fb0ff2399475584eb9f
ngomichael <michael_ngo@live.com>
2019-02-01 12:08:23 -0800

    removed styling in layout component

---
+++ src/components/Feature/Feature.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Feature.module.css'
+
+const Feature = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Feature
