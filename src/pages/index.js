90d62e519ba0f5647118a3febf019156646ef258
ngomichael <michael_ngo@live.com>
2019-02-01 12:08:23 -0800

    removed styling in layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+import Feature from '../components/Feature/Feature'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Feature />
   </Layout>
 )
 
