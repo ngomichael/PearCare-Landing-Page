38dd6bab83adf4900d22eb8596a7c2d4c5f42eab
ngomichael <michael_ngo@live.com>
2019-02-05 12:42:03 -0800

    created Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import styles from './index.module.css'
 
 import Layout from '../components/layout'
+import Hero from '../components/Hero/Hero'
+import ValueProps from '../components/ValueProps/ValueProps'
 import Features from '../components/Features/Features'
 import Members from '../components/Members/Members'
-import ValueProps from '../components/ValueProps/ValueProps'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className={styles.container}>
+      <section>
+        <Hero />
+      </section>
       <section className={styles.valueProps}>
         <ValueProps />
       </section>
