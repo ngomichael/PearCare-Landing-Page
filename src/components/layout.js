600ce81513bce6b9c0eabbcbea59bd1467a9336f
ngomichael <michael_ngo@live.com>
2019-02-05 15:29:53 -0800

    added link to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from './Header/header'
-import SocialSharing from '../components/SocialSharing/SocialSharing'
+import Footer from '../components/Footer/Footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
   <>
     <Header siteTitle="PearCare" />
     <div>{children}</div>
-    <SocialSharing />
+    <Footer />
   </>
 )
 
