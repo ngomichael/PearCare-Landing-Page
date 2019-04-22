366aeff3264b3185d46ca26d75a0ce5ef4b0696d
ngomichael <michael_ngo@live.com>
2019-04-22 15:02:00 -0700

    added hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import styles from './Hero.module.css'
+import { Button } from '../Button/Button'
 
 const Hero = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.sloganContainer}>
-        <h1 className={styles.slogan}>
-          Better patient-provider pairs, better mental healthcare
-        </h1>
+      <div className={styles.sloganButtonContainer}>
+        <div className={styles.sloganContainer}>
+          <h1 className={styles.slogan}>
+            Better patient-provider pairs, better mental healthcare
+          </h1>
+          <p className={styles.supplementaryText}>
+            At PearCare, we'll match you with providers that best fits your
+            lifestyle.
+          </p>
+        </div>
+        <Button>Find a provider</Button>
       </div>
     </div>
   )
 }
-
 export default Hero
